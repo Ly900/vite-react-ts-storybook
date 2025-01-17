f29498237cac5884d133d7665fed5bd9abdd7db3
Ly Nguyen <lynguyendc@gmail.com>
2025-01-17 17:55:37 -0500

    Simple build for SCSS to CSS

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,18 @@
+import terser from '@rollup/plugin-terser';
+// import scss from 'rollup-plugin-scss';
+import sass from 'rollup-plugin-sass';
+
 export default {
-	input: 'src/main.js',
-	output: {
-		file: 'bundle.js',
-		format: 'cjs',
-	},
+	input: './src/index.js',
+	output: [
+		{
+			file: './build/js/bundle.js',
+			format: 'esm',
+		},
+	],
+	plugins: [
+		sass({
+			output: './build/css/cmdr-global-styles.css',
+		}),
+	],
 };
